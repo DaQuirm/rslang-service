0962b36605ba32f96e2c5a40ccb1ba7398e648d1
DaQuirm <leo.daquirm@gmail.com>
2019-10-22 23:51:59 +0200

    Add languages

--- src/API/DBSchema.hs
+++ src/API/DBSchema.hs
@@ -5,11 +5,14 @@
 
 module API.DBSchema where
 
-import Data.Text (Text)
+import Data.Text (Text, pack)
+import qualified Data.ByteString.Lazy as ByteString
+import Data.Aeson (eitherDecode)
+import Control.Monad.IO.Class (liftIO)
 
 import Servant (type (:>), Post, JSON, Handler, (:<|>)(..))
 
-import Database.Selda (tryCreateTable)
+import Database.Selda (tryCreateTable, insert_)
 import Database.Selda.PostgreSQL (withPostgreSQL, on)
 
 import ServiceState (tick)
@@ -20,6 +23,7 @@ import Entity.User (usersTable)
 import Entity.Word (wordsTable)
 import Entity.WordSet (wordSetsTable)
 import Entity.WordSetWord (wordSetWordsTable)
+import Entity.Language (languagesTable)
 
 type DBSchemaAPI = "schema" :> "init" :> Post '[JSON] Text
 
@@ -35,5 +39,11 @@ dbSchemaAPI = initSchema
       tryCreateTable wordsTable
       tryCreateTable wordSetsTable
       tryCreateTable wordSetWordsTable
+      tryCreateTable languagesTable
 
-      pure "Initialisation complete"
+      decoded <- eitherDecode <$> (liftIO $ ByteString.readFile "./data/languages.json")
+      case decoded of
+        Left error      -> pure $ "Could not decode languages.json" <> (pack error)
+        Right languages -> do
+          insert_ languagesTable languages
+          pure "Initialisation complete"
---
+++ src/Entity/Language.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Entity.Language where
+
+import Prelude hiding (id)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import Database.Selda (ID, SqlRow, Table, table, Attr((:-)), unique, foreignKey)
+import Data.Aeson (ToJSON, FromJSON)
+
+data Language = Language
+  { code     :: Text
+  , name     :: Text
+  } deriving (Show, Generic)
+
+instance SqlRow Language
+instance ToJSON Language
+instance FromJSON Language
+
+languagesTable :: Table Language
+languagesTable = table "languages" [ #code :- unique ]
